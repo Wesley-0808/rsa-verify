dc7dbcaa2832f52b4aa82e30bf1b4e2aca57d555
Wesley <985189328@qq.com>
2025-12-16 20:10:44 +0800

    chore: build

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+// rollup.config.js
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default [
+  // CommonJS
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/cjs/index.cjs',
+      format: 'cjs',
+      exports: 'named'
+    },
+    plugins: [
+      nodeResolve(),
+      typescript({
+        tsconfig: './tsconfig.build.json',
+        compilerOptions: {
+          declaration: false,
+          declarationMap: false,
+          emitDeclarationOnly: false,
+          outDir: 'dist/cjs'
+        }
+      })
+    ],
+    external: ['crypto']
+  },
+
+  // ES Module
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/mjs/index.mjs',
+      format: 'esm'
+    },
+    plugins: [
+      nodeResolve(),
+      typescript({
+        tsconfig: './tsconfig.build.json',
+        compilerOptions: {
+          declaration: false,
+          declarationMap: false,
+          emitDeclarationOnly: false,
+          outDir: 'dist/mjs'
+        }
+      })
+    ],
+    external: ['crypto']
+  }
+];
